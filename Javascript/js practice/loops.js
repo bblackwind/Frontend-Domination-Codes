a3b1fa4ff0e96c51e809036acfc0de74b5743220
bblackwind <vishal230396@gmail.com>
2025-08-06 19:27:06 +0530

    for code

---
+++ Javascript/js practice/loops.js
@@ -0,0 +1,41 @@
+// 1 to 10
+
+console.log("1 to 10");
+for( let i =1 ; i<=10 ; i++){
+    console.log(i);    
+}
+
+console.log("--------------------------------------------");
+
+// 25 to 50
+console.log("25 to 50");
+for( let i =25 ; i<=50 ; i++){
+    console.log(i);    
+}
+
+console.log("--------------------------------------------");
+
+// 35 to 40
+console.log("35 to 40");
+for( let i =35 ; i<=40 ; i++){
+    console.log(i);    
+}
+
+console.log("--------------------------------------------");
+
+// 20 to 5
+console.log("20 to 5");
+for( let i =20 ; i>=5 ; i--){
+    console.log(i);    
+}
+
+console.log("--------------------------------------------");
+
+// 5 to 1
+console.log("5 to 1");
+for( let i =5 ; i>0 ; i--){
+    console.log(i);    
+}
+
+console.log("--------------------------------------------");
+
